d3fdc7f55b5462a38804bf66fc51c6a898726dc4
bmit-elc <chaimaa.eljarite@espas.ch>
2024-01-17 14:56:59 +0100

    espas logo mit text als hero mit "Willkomme.n..." texz in welcome blade hinzufügt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.app')
 @section('content')
-    <div>
-       <div>
-        <!-- ESPAS HEADER PICTURES CALL REPORT -->
-        Willkomen in ESPAS call report
-       </div>
+    <div class="px-4 py-5 my-5 text-center">
+        <img class="d-block mx-auto mb-4" src="img/header-logo.svg" alt="ESPAS Logo mit text Nah am Mensch. Nah am Markt" width="520" >
+        <h1 class="display-5 fw-bold">Willkomen bei der ESPAS Call Report</h1>
     </div>
 @endsection
