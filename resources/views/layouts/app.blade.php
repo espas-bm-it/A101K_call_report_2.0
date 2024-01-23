c60f80df0e60f189425051bd0d7c0d4b5ea6827c
bmit-elc <chaimaa.eljarite@espas.ch>
2024-01-23 11:51:12 +0100

    logo bug fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <a class="navbar-brand" href="{{ url('/home') }}">
+                            <img src="{{ asset('img/espas_logo.svg') }}" alt="ESPAS Logo" width="120" height="54">
+                        </a>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
