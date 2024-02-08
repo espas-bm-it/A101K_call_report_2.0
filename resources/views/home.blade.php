3698f804298857204a8e6cc2413063801d3ddf84
bmit-elc <chaimaa.eljarite@espas.ch>
2024-02-08 09:12:56 +0100

    Erste commit zur erstellung von date range filter. Änderungen in home.blade und main class "container" umbennent

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-10">
-                {!! $dataTable->table(['class' => 'table table-bordered']) !!}
+    <div class="card">
+        <div class="card-header">
+            <div class="col col-9">
+            </div>
+
+            <div class="col col-3">
+
+                <!--DATE RANGE FILTER -->
+                <div id="daterange" class="float-end"
+                    style="background: #ffff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 100%; text-align:center">
+                    <i class="fa fa-calendar"></i>&nbsp;
+                    <span></span>
+                    <i class="fa fa-caret-down"></i>
+                </div>
+                <!--        ------         -->
+            </div>
+            <div class="card-body">
+                {!! $dataTable->table(['class' => 'table table-bordered', 'id' => 'daterange_table']) !!}
             </div>
         </div>
-    @endsection
+    </div>
+@endsection
 
-    @push('scripts')
-        {!! $dataTable->scripts() !!}
-    @endpush
+
+@push('scripts')
+    {!! $dataTable->scripts() !!}
+@endpush
